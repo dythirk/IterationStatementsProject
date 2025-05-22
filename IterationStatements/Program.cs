6206329f1615eb0492fb7ded868e2ce3aa797d50
Thai Vo <tahrbo@gmail.com>
2025-05-22 03:19:55 -0500

    Testing

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -85,7 +85,7 @@ namespace IterationStatements
             //    - Less than or equal to the number of items in "numbers" (use "numbers.Count")
             //    - Greater than or equal to 0
             // c) Decrement "i" by 1 in each iteration
-
+            for (int i = 199; (i <= numbers.Count) && (i >= 0); i--)
             {
                 //TODO - Inside the loop, place numbers[i] inside of the Console.WriteLine() method
                 Console.WriteLine(numbers[i]); // Example placement of numbers[i] inside Console.WriteLine
